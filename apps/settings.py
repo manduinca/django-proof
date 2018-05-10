e3c6c12356b50cb3df6c2b0309eb19d0bd80b8cf
manduinca <jeanpierre.mandujano@gmail.com>
2018-05-10 03:24:21 -0500

    add configuration y folder content for templates

--- apps/settings.py
+++ apps/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'apps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['apps/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
