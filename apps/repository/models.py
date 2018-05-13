7a47ac1ac268c7ecbe9c03005ac25d9310155437
manduinca <jeanpierre.mandujano@gmail.com>
2018-05-13 14:19:05 -0500

    Add models Organization and Repository

--- apps/repository/models.py
+++ apps/repository/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+
+    name = models.CharField(
+        max_length=100,
+    )
+
+    login = models.CharField(
+        max_length=100,
+    )
+
+    url = models.URLField()
+
+    location = models.CharField(
+        max_length=200,
+    )
+
+    created_at = models.DateTimeField()
+
+    updated_at = models.DateTimeField()
+
+
+class Repository(models.Model):
+
+    organization = models.ForeignKey(
+        Organization,
+    )
+
+    github_id = models.PositiveIntegerField()
+
+    name = models.CharField(
+        max_length = 100,
+    )
+
+    url = models.URLField()
+
+    last_commit = models.DateTimeField()
+
+    commits_url = models.URLField()
+
+    created_at = models.DateTimeField()
+
+    updated_at = models.DateTimeField()
